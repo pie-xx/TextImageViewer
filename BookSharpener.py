b459e1597fca8aa3c2c7c3e73e72d6f413f5ef2f
Kajikawa, Takafumi <piecrosoft@gmail.com>
2020-07-11 08:51:58 +0900

    sharpener() が不要なので除去 getUmed() で求める白黒の境をメジアンに戻した 白地とみなす分散の値を可変で求めるようにした。ヒストグラムのピークから登ったところに設定。 白、黒を強調するための係数を変数として定義。後で自動設定できるようにする。

--- BookSharpener.py
+++ BookSharpener.py
@@ -2,53 +2,71 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 
-Testimagefile = 'testimage.jpg'
-Blocksize = 64
+Testimagefile = 'city.jpg'
+TestimageTitle = Testimagefile.split('.')[0]
+Blocksize = 32
+Slim = 5.0
+Wbias = 1.5
+Bbias = 0.3
+
+def stdmap(img):
+    stds = []
+    for y in range( 0, img.shape[0], Blocksize ):
+        for x in range( 0, img.shape[0], Blocksize ):
+            pimg = img_gray[y:y+Blocksize, x:x+Blocksize]
+            std = np.std( pimg )
+            minv = np.min( pimg )
+            maxv = np.max( pimg )
+            stds.append(std)
+    hist = np.histogram( stds, bins=64 )
+    print( hist[0] )
+    print(np.argmax(hist[0]))
+    print( hist[1] )
+    peaki = np.argmax(hist[0])
+    if peaki == 0:
+        peaki = 1
+    for n in range(peaki,len(hist[0])):
+        if hist[0][n-1] < hist[0][n]:
+            Slim = hist[1][n+1]
+            break
+    print(Slim)
+    plt.hist(stds, 100, [0, 100])
+    plt.show()
 
 def getUmed(img):
     med = np.median(img) 
     fild = img[img < med]
     umed = np.median(fild) 
-    #fild2 = fild[fild < umed]
-    #umed = np.median(fild2) 
-    #fild3 = fild2[fild2 < umed]
-    #umed = np.median(fild3) 
     return( umed )
-
-def sharpener( img_gray, slim, pname ):
-    
-    dst=img_gray.copy()
-    for y in range(img_gray.shape[0]):
-        for x in range(img_gray.shape[1]):
-            if dst[y][x] > slim:
-                dst[y][x] = 255
-            else:
-                dst[y][x] = dst[y][x] / 2
-
-    cv2.imwrite(pname+'{:03d}.jpg'.format(slim), dst )
 
 bookimg = cv2.imread( Testimagefile )
 img_gray = cv2.cvtColor(bookimg, cv2.COLOR_BGR2GRAY)
-"""
-clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(Blocksize,Blocksize))
-cl1 = clahe.apply(img_gray)
-cv2.imwrite('testimage_cv.jpg', cl1 )
 
-"""
 imgHeight = int(img_gray.shape[0])
 imgWidth = int(img_gray.shape[1])
 
+stdmap(img_gray)
+
+
 for y in range( 0, imgHeight, Blocksize ):
+    print( y )
     for x in range( 0, imgWidth, Blocksize ):
         pimg = img_gray[y:y+Blocksize, x:x+Blocksize]
         std = np.std( pimg )
         minv = np.min( pimg )
         maxv = np.max( pimg )
+        avr = np.average(pimg)
+        #pimg = cv2.equalizeHist(pimg)
         pimg -= minv
 
-        slim = getUmed( pimg )
-        print( y, x, slim, std, minv, maxv )
-        if std < 6.0:
+        wlim = getUmed( pimg )
+
+        """
+        for sy in range (pimg.shape[0]):
+            for sx in range( pimg.shape[1] ):
+                img_gray[y+sy][x+sx] = std*4
+        """
+        if std < Slim:
             for sy in range (pimg.shape[0]):
                 for sx in range( pimg.shape[1] ):
                     img_gray[y+sy][x+sx] = 255
@@ -56,17 +74,15 @@ for y in range( 0, imgHeight, Blocksize ):
             for sy in range (pimg.shape[0]):
                 for sx in range( pimg.shape[1] ):
                     if maxv != minv:
-                        img_gray[y+sy][x+sx] = int((img_gray[y+sy][x+sx] *255.0)/(maxv - minv))                    
-                    if pimg[sy][sx] > slim:
-                    #    img_gray[y+sy][x+sx] = 256 - (256 - img_gray[y+sy][x+sx]) / 2
+                        img_gray[y+sy][x+sx] = (img_gray[y+sy][x+sx]*255.0)/(maxv - minv)
+                    if pimg[sy][sx] > wlim:
                         v = img_gray[y+sy][x+sx]
-                        v = v * 2
-                        if v > 256:
+                        v = v * Wbias
+                        if v > 255:
                             v = 255
                         img_gray[y+sy][x+sx] = v
                     else:
-                        img_gray[y+sy][x+sx] = pimg[sy][sx] / 3
-     
-cv2.imwrite('testimage_f.jpg', img_gray )
-cv2.imshow('testimage_f1', img_gray )
+                        img_gray[y+sy][x+sx] = pimg[sy][sx] * Bbias
+
+cv2.imwrite(TestimageTitle+'_mf152.jpg', img_gray )
 
